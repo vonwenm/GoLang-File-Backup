7cb5c7922258c15e8d0bdf436b34037799e8161b
gd <gregdzezinski@gmail.com>
2014-02-11 22:06:03 -0500

    error handling of args

--- backup.go
+++ backup.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
-import "github.com/gallend/golang-file-backup/DirectoryReader"
+import (
+	"errors"
+	"fmt"
+	"github.com/gallend/golang-file-backup/DirectoryReader"
+	"log"
+	"os"
+)
+
+var (
+	ErrInvalidArg = errors.New("Missing one or more arguments")
+)
 
 func main() {
-	s := DirectoryReader.ReadAll("C:\\Users\\gregd_000\\Pictures\\Emily")
+
+	if len(os.Args) != 3 {
+		log.Fatal(ErrInvalidArg)
+	}
+
+	fmt.Println(len(os.Args))
+
+	originDirectory := os.Args[1]
+	// backupDirectory := os.Args[1]
+
+	s := DirectoryReader.ReadAll(originDirectory)
 
 	for i := 0; i < 2; i++ {
 		fmt.Println(s[i].Name())
